64008e5fa55eb3ea63448fb4e6611803cba8789a
adamdgit <adam_d@outlook.com>
2023-05-12 16:43:48 +1000

    Save tokens to localstorage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { redirect } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import { useEffect } from 'react';
 
 type searchParamsTypes = {
   searchParams: {
@@ -11,17 +11,22 @@ type searchParamsTypes = {
   }
 }
 
-export default function Home( { searchParams } : searchParamsTypes) {
-
-  const [accessToken, setAccessToken] = useState(searchParams.access_token)
-  const [refreshToken, setRefreshToken] = useState(searchParams.refresh_token)
-  const [expiresIn, setExpiresIn] = useState(searchParams.expires_in)
+export default async function Home({ searchParams } : searchParamsTypes) {
 
   useEffect(() => {
-    if (!accessToken && !refreshToken) {
+
+    localStorage.setItem("spotify_access_token", searchParams.access_token)
+    localStorage.setItem("spotify_refresh_token", searchParams.refresh_token)
+    localStorage.setItem("spotify_expires_in", searchParams.expires_in)
+  
+    if (!searchParams.access_token && !searchParams.refresh_token) {
       redirect("/");
+    } else {
+      window.history.replaceState(null, '', "http://localhost:3000/home")
     }
+
   },[])
+
 
   return (
     <div>test</div>
