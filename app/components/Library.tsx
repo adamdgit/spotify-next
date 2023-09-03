b3b2f93021721d634919d62161de914e09885dd8
adamdgit <adam_d@outlook.com>
2023-09-03 14:39:51 +1000

    Created library component

---
+++ app/components/Library.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useSession } from "next-auth/react";
+import { useContext, useEffect } from "react";
+import { GlobalStateContext, contextProps } from "../providers";
+import { spotifyAPI } from "../api/auth/[...nextauth]/route";
+import styles from './styles.module.css'
+
+export default function Library() {
+
+  const { data: session } = useSession();
+  const { usersPlaylists, setUsersPlaylists } = useContext(GlobalStateContext) as contextProps
+
+  async function getPlaylists() {
+    if (session?.accessToken) {
+      const data = await spotifyAPI.getUserPlaylists();
+      setUsersPlaylists(data.body.items);
+      console.log(data)
+    } else {
+      console.error("no access token");
+    }
+  }
+
+  useEffect(() => {
+    if (!session) return
+    getPlaylists();
+  },[session])
+
+  return (
+    <main className={styles.mainContent}>
+      <h1>Welcome {session?.user?.name}</h1>
+
+      <ul className={styles.playlists}>
+        {
+          usersPlaylists.map((item, index) => (
+            <li key={index}>
+              <p>{item?.name}</p>
+              <img src={item.images[1].url} alt={item.name + 'playlist art'} />
+            </li>
+          ))
+        }
+      </ul>
+    </main>
+  )
+}
