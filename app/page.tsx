e19fdd1215b6638b39baa550c26529117bcdaffe
adamdgit <adam_d@outlook.com>
2023-09-03 14:37:50 +1000

    setup access token & display page contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,41 @@
 'use client'
 
-import { signOut, useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
+import Library from './components/Library';
+import Home from './components/Home';
+import Search from './components/Search';
+import { useContext, useEffect, useState } from 'react';
+import Header from './components/Header';
+import Player from './components/Player';
+import { spotifyAPI } from './api/auth/[...nextauth]/route';
 
 export default function Login() {
 
-  const session = useSession();
+  const { data: session } = useSession();
+  const [navigation, setNav] = useState(0);
+
+  async function setupSpotifyAPI() {
+
+    if (!session?.accessToken) return
+    // provide spotifyAPI library with access token
+    await spotifyAPI.setAccessToken(session?.accessToken);
+  }
+
+  useEffect(() => {
+    setupSpotifyAPI()
+  },[session])
 
   return (
-    <main>
-      <button onClick={() => signOut()}>Signout</button>
-      <h1>Logged in</h1>
-      <p>{session.data?.accessToken}</p>
-      <p>{session.data?.expires}</p>
-      <p>{session.data?.userId}</p>
-      <p>{session.data?.user?.name}</p>
-    </main>
+    <>
+    <Header setNav={setNav} />
+    {
+      navigation === 0 ? <Home />
+      : navigation === 1 ? <Search />
+      : <Library />
+    }
+    {
+      session?.accessToken && <Player accessToken={session?.accessToken} />
+    }
+    </>
   )
 }
