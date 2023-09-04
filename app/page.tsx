147e74f8f499ae417e51022052e320854807aebe
adamdgit <adam_d@outlook.com>
2023-09-04 12:40:39 +1000

    Fixed conditional rendering of body Elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,67 @@
 'use client'
 
-import { useSession } from 'next-auth/react'
-import Library from './components/Library';
-import Home from './components/Home';
-import Search from './components/Search';
-import { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react'
+import { useSession } from 'next-auth/react';
+import { GlobalStateContext, contextProps } from './providers';
+import { spotifyAPI } from './api/auth/[...nextauth]/route';
 import Header from './components/Header';
 import Player from './components/Player';
-import { spotifyAPI } from './api/auth/[...nextauth]/route';
+import Search from './components/Search';
+import Library from './components/Library';
 
-export default function Login() {
+export default function Home() {
 
-  const { data: session } = useSession();
+  const session = useSession();
   const [navigation, setNav] = useState(0);
+  const [topArtists, setTopArtists] = useState([])
+
+  const { deviceId } = useContext(GlobalStateContext) as contextProps
 
   async function setupSpotifyAPI() {
-
-    if (!session?.accessToken) return
+    if (!session?.data.accessToken) return
     // provide spotifyAPI library with access token
-    await spotifyAPI.setAccessToken(session?.accessToken);
+    await spotifyAPI.setAccessToken(session?.data.accessToken)
+
+    if (!deviceId) return
+    // device randomly disconnects, this should reset playback
+    await spotifyAPI.transferMyPlayback([deviceId]);
+  }
+
+  async function getTopArtists() {
+    const data = await spotifyAPI.getMyTopArtists();
+    setTopArtists(data.body.items)
+    console.log(data.body.items)
   }
 
   useEffect(() => {
-    setupSpotifyAPI()
+    if (session.data?.accessToken) {
+      setupSpotifyAPI()
+      console.log("session is ready")
+      getTopArtists()
+    }
   },[session])
 
   return (
     <>
-    <Header setNav={setNav} />
+    <Header setNav={setNav}/>
     {
-      navigation === 0 ? <Home />
-      : navigation === 1 ? <Search />
+      !session ? <div></div>
+      : navigation === 0 ? 
+      <div>
+        {
+          topArtists.length > 0 &&
+          topArtists.map((item, idx) => {
+            return <li key={idx}>{item.name}</li>
+          })
+        }
+      </div>
+      : navigation === 1 ? 
+      <Search />
       : <Library />
     }
-    {
-      session?.accessToken && <Player accessToken={session?.accessToken} />
-    }
+    <Player />
     </>
+
   )
 }
+
