110b3f911eec3f39e661e05dcae4d8272513b14c
adamdgit <adam_d@outlook.com>
2023-09-09 01:05:25 +1000

    Fixed classnames in loading component

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -1,11 +1,12 @@
+import styles from "./styles.module.css"
 
 export default function Loading({ ...props }) {
   return (
-    <span className="loading">
+    <span className={styles.loading}>
       <p>{props.loadingMsg}</p>
       <svg xmlns="http://www.w3.org/2000/svg" 
         viewBox="-11.5 -10.23174 23 20.46348" 
-        width="50px" height="50px" className="react-logo">
+        width="50px" height="50px" className={styles.reactLogo}>
         <circle cx="0" cy="0" r="2.05" fill="#61dafb"/>
         <g stroke="#61dafb" strokeWidth="1" fill="none">
           <ellipse rx="11" ry="4.2"/>
