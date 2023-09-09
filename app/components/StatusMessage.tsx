a41f54aa5acba75edbf4e4acf82594886f26ee43
adamdgit <adam_d@outlook.com>
2023-09-09 11:34:35 +1000

    Added state and effects for status message

--- app/components/StatusMessage.tsx
+++ app/components/StatusMessage.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useContext, useState, useEffect } from "react";
+import styles from "./styles.module.css"
+import { GlobalStateContext, contextProps } from "../providers";
 
 export default function StatusMessage() {
+
+  const [showMsg, setShowMsg] = useState(false)
+  const { message } = useContext(GlobalStateContext) as contextProps
+
+  // when global message state changs, show message for 1.5secs
+  useEffect(() => {
+
+    if (message.needsUpdate === true) {
+      setShowMsg(true)
+      setTimeout(() => {
+        setShowMsg(false)
+      }, 1500)
+    }
+
+  },[message])
+
   return (
-    <div>StatusMessage</div>
+    <div className={showMsg === true ? `${styles.playlistUpdateMessage} ${styles.show}`
+      : styles.playlistUpdateMessage}>
+      <span>{message.msg}</span>
+      <span className={styles.triangle}></span>
+    </div>
   )
+
 }
