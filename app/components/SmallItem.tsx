a30629066364f1352cf0cb900502bbea0c02dd49
adamdgit <adam_d@outlook.com>
2023-09-24 22:03:00 +1000

    Wrap items in button to play context onclick

--- app/components/SmallItem.tsx
+++ app/components/SmallItem.tsx
@@ -1,14 +1,23 @@
 import styles from "./styles.module.css"
+import { spotifyAPI } from "../api/auth/[...nextauth]/route"
 
 export default function SmallItem({ ...props }) {
   return (
-    <li className={styles.homeItem}>
-      <img 
-        width={100}
-        height={100}
-        src={props.item.images[2] ? props.item.images[2].url : "no image data"} 
-        alt={props.item.name + 'art'} />
-        <span>{props.item.name}</span>
+    <li>
+      <button 
+      className={styles.homeItem} 
+      onClick={() => spotifyAPI.play({'context_uri': props.item.uri})}>
+        <img 
+            width={100}
+            height={100}
+            src={
+              props.item.images.length > 1 ? props.item.images[2].url 
+              : props.item.images.length === 1 ? props.item.images[0].url
+              : "no image data"
+            } 
+            alt={props.item.name + 'art'} />
+          <span>{props.item.name}</span>
+      </button>
     </li>
   )
 }
