65fd7e98f057fb9910864a6a287715fdb2ee14a5
adamdgit <adam_d@outlook.com>
2023-09-24 22:18:48 +1000

    dont show msg if empty string

--- app/components/StatusMessage.tsx
+++ app/components/StatusMessage.tsx
@@ -10,19 +10,19 @@ export default function StatusMessage() {
   // when global message state changs, show message for 1.5secs
   useEffect(() => {
 
-    if (message.needsUpdate === true) {
-      setShowMsg(true)
-      setTimeout(() => {
-        setShowMsg(false)
-      }, 1500)
-    }
+    if (!message) return
+    
+    setShowMsg(true)
+    setTimeout(() => {
+      setShowMsg(false)
+    }, 1500)
 
   },[message])
 
   return (
     <div className={showMsg === true ? `${styles.playlistUpdateMessage} ${styles.show}`
       : styles.playlistUpdateMessage}>
-      <span>{message.msg}</span>
+      <span>{message}</span>
       <span className={styles.triangle}></span>
     </div>
   )
