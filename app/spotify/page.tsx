dfdb5fbe158a91863caf97cb360d9319bfac1cf1
adamdgit <adam_d@outlook.com>
2023-11-30 14:03:51 +1100

    Moved SmallItem component to within parent

--- app/spotify/page.tsx
+++ app/spotify/page.tsx
@@ -5,7 +5,28 @@ import { spotifyAPI } from '../api/auth/[...nextauth]/route';
 import { useSession } from 'next-auth/react';
 import styles from "@/app/components/styles.module.css"
 import { GlobalStateContext, contextProps } from '../providers';
-import SmallItem from '@/app/components/SmallItem';
+import useFetch from '../hooks/useFetch';
+
+function SmallItem({ item }: any) {
+  return (
+    <li>
+      <button 
+      className={styles.homeItem} 
+      onClick={() => spotifyAPI.play({'context_uri': item.uri})}>
+        <img 
+            width={100}
+            height={100}
+            src={
+              item.images.length > 1 ? item.images[2].url 
+              : item.images.length === 1 ? item.images[0].url
+              : "no image data"
+            } 
+            alt={item.name + 'art'} />
+          <span>{item.name}</span>
+      </button>
+    </li>
+  )
+}
 
 export default function Spotify() {
   
@@ -51,46 +72,40 @@ export default function Spotify() {
     }
     getFeatured()
       .catch(err => console.error(err))
-      
 
   },[session])
 
+
   return (
-    <>
-    <main className={styles.mainContent}>
-      <h1>Your Playlists</h1>
-      <ul className={styles.homeItemList}>
-        {
-        sortedPlaylists.length > 0 ?
-        sortedPlaylists.map((item, idx) => 
-          <SmallItem key={idx} item={item} />
-        )
-        : <p>No playlists found.</p>
-        }
-      </ul>
+    <div className={styles.pageWrap}>
+      <main className={styles.mainContent}>
+        <h1>Your Playlists</h1>
+        <ul className={styles.homeItemList}>
+          {sortedPlaylists.length > 0 ?
+          sortedPlaylists.map((item, idx) => 
+            <SmallItem key={idx} item={item} />
+          )
+          : <p>No playlists found.</p>}
+        </ul>
 
-      <h2>Your Top Artists</h2>
-      <ul className={styles.homeItemList}>
-        {
-        topArtists ?
-        topArtists.map((item, idx) => 
-          <SmallItem key={idx} item={item} />
-        )
-        : <p>Loading artists..</p>
-        }
-      </ul>
+        <h2>Your Top Artists</h2>
+        <ul className={styles.homeItemList}>
+          {topArtists ?
+          topArtists.map((item, idx) => 
+            <SmallItem key={idx} item={item} />
+          )
+          : <p>Loading artists..</p>}
+        </ul>
 
-      <h2>Featured Playlists</h2>
-      <ul className={styles.homeItemList}>
-        {
-        featuredPlaylists ?
-        featuredPlaylists.map((item, idx) => 
-          <SmallItem key={idx} item={item} />
-        )
-        : <p>Loading featured playlists..</p>
-        }
-      </ul>
-    </main>
-    </>
+        <h2>Featured Playlists</h2>
+        <ul className={styles.homeItemList}>
+          {featuredPlaylists ?
+          featuredPlaylists.map((item, idx) => 
+            <SmallItem key={idx} item={item} />
+          )
+          : <p>Loading featured playlists..</p>}
+        </ul>
+      </main>
+    </div>
   )
 }
