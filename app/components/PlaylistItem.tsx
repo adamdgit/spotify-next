60e271789167a3440750aba32e61360e9165b614
adamdgit <adam_d@outlook.com>
2023-11-30 14:01:38 +1100

    Added button styles

--- app/components/PlaylistItem.tsx
+++ app/components/PlaylistItem.tsx
@@ -24,17 +24,15 @@ export default function PlaylistItem({ ...props }) {
             <p>{props.item.description}</p>
             {
             props.item.owner.id !== props.session?.userId 
-            ? <button onClick={() => spotifyAPI.unfollowPlaylist(props.item.id)}>
+            ? <button className={styles.button1} onClick={() => spotifyAPI.unfollowPlaylist(props.item.id)}>
                   UnFollow
               </button>
             : 
             <span className={styles.resultBtns}>
-              <button 
-                onClick={() => router.push(`/spotify/editplaylist/${encodeURIComponent(props.item.id)}`)}
-                className={styles.editLink}>
+              <button onClick={() => router.push(`/spotify/editplaylist/${encodeURIComponent(props.item.id)}`)} className={styles.button1}>
                 Edit
               </button>
-              <button onClick={() => spotifyAPI.unfollowPlaylist(props.item.id)}>
+              <button className={styles.button1} onClick={() => spotifyAPI.unfollowPlaylist(props.item.id)}>
                   Delete
               </button>
             </span>
